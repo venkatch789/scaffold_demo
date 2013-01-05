f4eda6af0c280b656428af3e524153a4fdc6b012
venkat <venkat.ch.bi@gmail.com>
2013-01-05 17:22:49 +0530

    fixed root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ScaffoldDemo::Application.routes.draw do
   resources :products
 
-
+  root :to => 'products#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
